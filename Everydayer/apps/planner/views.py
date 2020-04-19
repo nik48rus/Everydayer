3b48eb1be7eb9ec9202217737e64d086e5f1aa1d
Nikita <jaragent@mail.ru>
2020-04-19 10:53:45 +0300

    delete comments

--- Everydayer/apps/planner/views.py
+++ Everydayer/apps/planner/views.py
@@ -5,14 +5,12 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse
 from Everydayer.apps.planner.models import Task
 from django.urls import reverse
 from datetime import timedelta
-from Everydayer.apps.planner.forms import EditForm
 
 month_list = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь',
               'Декабрь']
 
 
 def index(request):
-    # latest_tasks = Task.objects.order_by('-task_date')
     today = datetime.date.today()
     week = [today, today + timedelta(1), today + timedelta(2), today + timedelta(3), today + timedelta(4),
             today + timedelta(5), today + timedelta(6), today + timedelta(7)]
